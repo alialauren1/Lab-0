7011a8c59ed9fa3449d492854316def3b5f9a313
evoeddie <157066050+evoeddie@users.noreply.github.com>
2024-02-01 08:19:14 -0800

    Lab0: Doc, src

---
+++ lab0/docs/navtreeindex0.js
@@ -0,0 +1,12 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[1,0,0],
+"files.html":[1,0],
+"index.html":[],
+"lab0_8py.html":[1,0,0,0],
+"lab0_8py.html#a309bc09e2546a503f3bd80af7c4e455a":[1,0,0,0,1],
+"lab0_8py.html#a9ba8c892e06ccc7e8256fa4c3556c94f":[1,0,0,0,0],
+"namespace_m_e405___lab0___wk2___rev_a.html":[0,0,0],
+"namespaces.html":[0,0],
+"pages.html":[]
+};
---
+++ lab0/docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['plot_5fexample_0',['plot_example',['../lab0_8py.html#a9ba8c892e06ccc7e8256fa4c3556c94f',1,'lab0']]]
+];
