eca1da4e1d33c857c1f79691b87b47b0af31ad38
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2024-02-01 10:31:04 -0800

    Update lab0.py

--- src/lab0.py
+++ src/lab0.py
@@ -8,7 +8,7 @@ default.
 This file is based loosely on an example found at
 https://matplotlib.org/stable/gallery/user_interfaces/embedding_in_tk_sgskip.html
 
-@author Spluttflob
+@author Alia Wolken, Eduardo Santos, Andrew Jwaideh
 @date   2023-12-24 Original program, based on example from above listed source
 @copyright (c) 2023 by Spluttflob and released under the GNU Public Licenes V3
 """
